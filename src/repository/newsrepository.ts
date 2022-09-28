b9a73fb8f081d3aa8c953ad9ed4fe9173976bf49
Andreeas Firoiu <andreeas.firoiu@liantis.be>
2022-09-28 13:38:02 +0200

    omzetten repoconstructen naar echte classes

--- src/repository/newsrepository.ts
+++ src/repository/newsrepository.ts
@@ -1,34 +1,30 @@
 import { NewsCreateDto } from "../entity/dto/news/news.create.dto"
 import { NewsUpdateDto } from "../entity/dto/news/news.update.dto"
 import { Logger } from "../util/logging";
-const logger = new Logger();
 
-const findAll = async () => {
+export class NewsRepository {
+
+logger = new Logger();
+
+findAll = async () => {
   throw new Error("Not implemented yet.")
   
 }
 
-const findById = async (id: number) => {
+findById = async (id: number) => {
   throw new Error("Not implemented yet.")
 }
 
-const create = async (dto: NewsCreateDto) => {
+create = async (dto: NewsCreateDto) => {
   throw new Error("Not implemented yet.")
 }
 
-const updateById = async (Id: number, dto: NewsUpdateDto) => {
+updateById = async (Id: number, dto: NewsUpdateDto) => {
   throw new Error("Not implemented yet.")
 }
 
-const deleteById =async (id:number) => {
+deleteById =async (id:number) => {
   throw new Error("Not implemented yet.")
 }
 
-
-module.exports = {
-  findAll,
-  findById,
-  create,
-  updateById,
-  deleteById,
-};
+}
--- src/repository/userrepository.ts
+++ src/repository/userrepository.ts
@@ -1,34 +1,30 @@
 import { SiteUserCreateDto } from "../entity/dto/siteuser/siteuser.create.dto"
 import { SiteUserUpdateDto } from "../entity/dto/siteuser/siteuser.update.dto"
 import { Logger } from "../util/logging";
-const logger = new Logger();
 
-const findAll = async () => {
+export class UserRepository{
+
+logger = new Logger();
+
+findAll = async () => {
   throw new Error("Not implemented yet.")
   
 }
 
-const findById = async (id: number) => {
+findById = async (id: number) => {
   throw new Error("Not implemented yet.")
 }
 
-const create = async (dto: SiteUserCreateDto) => {
+create = async (dto: SiteUserCreateDto) => {
   throw new Error("Not implemented yet.")
 }
 
-const updateById = async (Id: number, dto: SiteUserUpdateDto) => {
+updateById = async (Id: number, dto: SiteUserUpdateDto) => {
   throw new Error("Not implemented yet.")
 }
 
-const deleteById =async (id:number) => {
+deleteById =async (id:number) => {
   throw new Error("Not implemented yet.")
 }
+}
 
-
-module.exports = {
-  findAll,
-  findById,
-  create,
-  updateById,
-  deleteById,
-};
