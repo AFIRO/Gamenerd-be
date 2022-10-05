75f18227491e4d1c16ad06cae2f82e7a7ee7e472
Andreeas FIroiu <andrfiro@gmail.com>
2022-10-05 22:35:26 +0200

    vuilnis buiten

--- src/testing/prisma.mock.ts
+++
@@ -1,8 +0,0 @@
-import { mockDeep } from 'jest-mock-extended';
-import { PrismaClient as OriginalPrismaClient } from '@prisma/client';
-
-jest.mock('@prisma/client', () => ({
-  PrismaClient: function () {
-    return mockDeep<OriginalPrismaClient>();
-  }
-}));
